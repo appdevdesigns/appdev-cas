9b1ba15ba2ad24384f4861f1f3bd064d3ef736dd
Johnny Hausman <johnny@appdevdesigns.net>
2014-09-12 15:11:28 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+appdev-cas ![Build Status](https://travis-ci.org/appdevdesigns/appdev-cas.png)
+================
+
+A new plugin.
---
+++ config/routes.js
@@ -0,0 +1,29 @@
+/**
+ * Routes
+ *
+ * Use this file to add any module specific routes to the main Sails
+ * route object.
+ */
+
+
+module.exports = {
+
+
+  /*
+
+  '/': {
+    view: 'user/signup'
+  },
+  '/': 'appdev-cas/PluginController.inbox',
+  '/': {
+    controller: 'appdev-cas/PluginController',
+    action: 'inbox'
+  },
+  'post /signup': 'appdev-cas/PluginController.signup',
+  'get /*(^.*)' : 'appdev-cas/PluginController.profile'
+
+  */
+
+
+};
+
